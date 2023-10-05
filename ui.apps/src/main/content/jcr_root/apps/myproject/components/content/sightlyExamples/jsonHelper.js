7321a3caae8292dfbe859da4dbbbe47e617d6d73
ankLui <luitel789@gmail.com>
2023-10-05 23:17:14 +0530

    Sightly Examples

---
+++ ui.apps/src/main/content/jcr_root/apps/myproject/components/content/sightlyExamples/jsonHelper.js
@@ -0,0 +1,10 @@
+use(function () {
+    var myData = {
+        str: "foo",
+        arr: [1, 2, 3]
+    };
+
+    return {
+        json: JSON.stringify(myData)
+    };
+});
---
+++ ui.apps/src/main/content/jcr_root/apps/myproject/components/content/sightlyExamples/sightlyExamplesHelper.js
@@ -0,0 +1,8 @@
+use(function(){
+'use strict';
+    return {
+        link: "#my Link",
+        title: "#my Title",
+        text: "#my Text"
+    }
+});
